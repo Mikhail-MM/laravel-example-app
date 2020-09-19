512628ef2d30c8521461f466c161bca560398058
Mikhail-MM <leakymee@bu.edu>
2020-09-18 17:30:00 -0700

    Added migrations

--- database/migrations/2020_09_19_002758_add_title_to_posts_table.php
+++ database/migrations/2020_09_19_002758_add_title_to_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration
+class AddTitleToPostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,9 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->id();
-            $table->string('slug');
-            $table->text('body');
-            $table->timestamps();
-            $table->timestamp('published_at')->nullable();
+        Schema::table('posts', function (Blueprint $table) {
+          // Add a column to the 'posts' table
+          $table->string('title');
         });
     }
 
@@ -29,6 +26,8 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::table('posts', function (Blueprint $table) {
+          $table->dropColumn('title');
+        });
     }
 }
