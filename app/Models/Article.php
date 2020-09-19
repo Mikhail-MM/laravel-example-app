4a17fbf3226f3ea77ddaeb9f10a6ec4294a778a4
Mikhail-MM <leakymee@bu.edu>
2020-09-19 16:46:15 -0700

    Added method to Article model to find all Articles written by that User, pass in user_id foreign key manually

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Models\User;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -23,7 +25,10 @@ class Article extends Model
       return route('articles.getById', $this->id);
     }
     public function getAuthorOfArticle() {
+      // You need to specify the foreign key name
+      // Because it infers from the METHOD NAME!
       
+      return $this->belongsTo('App\Models\User', 'user_id');
     }
     use HasFactory;
 }
