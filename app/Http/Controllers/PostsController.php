88eb63e273394a1d96519fb3eab60b6118fa8f57
Mikhail-MM <leakymee@bu.edu>
2020-09-18 17:20:17 -0700

    In-Line Error Handling

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,17 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
   public function render($slug) {
-    $post = Post::where('slug', $slug)->first();
+    $post = Post::where('slug', $slug)->firstOrFail();
     
-    if (!$post) {
-      abort(404);
-    }
     return view('db-post', [
       'post' => $post->body
     ]);
